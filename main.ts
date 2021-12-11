98464b2720491ac097fec298f2fea7202d43e8b1
vr2lousir <68815618+vr2lousir@users.noreply.github.com>
2021-12-11 16:22:34 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,72 @@
+let direction = 0
+input.onButtonPressed(Button.AB, function () {
+    input.calibrateCompass()
+})
+basic.forever(function () {
+    direction = input.compassHeading()
+    if (direction >= 338 || direction < 23) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (direction >= 23 && direction < 68) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+    } else if (direction >= 68 && direction < 113) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (direction >= 113 && direction < 158) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+    } else if (direction >= 158 && direction < 203) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (direction >= 203 && direction < 248) {
+        basic.showLeds(`
+            # . . . .
+            . # . . #
+            . . # . #
+            . . . # #
+            . # # # #
+            `)
+    } else if (direction >= 248 && direction < 293) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (direction >= 293 && direction < 338) {
+        basic.showLeds(`
+            . # # # #
+            . . . # #
+            . . # . #
+            . # . . #
+            # . . . .
+            `)
+    }
+})
